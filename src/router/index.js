ae14bd98f62cde10efa596adbb1302db9e5ea23f
Angel Velasquez <pcisavel@upc.edu.pe>
2024-04-18 16:58:15 -0500

    feat(in-app-navigation): configured navigation routes.

--- src/router/index.js
+++ src/router/index.js
@@ -4,10 +4,16 @@
  * @docs: https://router.vuejs.org/
  */
 import {createRouter, createWebHistory} from "vue-router";
+import HomeComponent from "../public/pages/home.component.vue";
+import AboutComponent from "../public/pages/about.component.vue";
 
 const router = createRouter({
     history: createWebHistory(),
-    routes: []
+    routes: [
+        { path: "/home",    component: HomeComponent, meta: { title: "Home"}},
+        { path: "/about",   component: AboutComponent, meta: { title: "About us"}},
+        { path: "/",        redirect: "/home"},
+    ]
 });
 
 export default router;
