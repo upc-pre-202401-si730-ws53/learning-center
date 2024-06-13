ccfa15d2137cb706cb0554831e2555236c0686f2
Angel Velasquez <pcisavel@upc.edu.pe>
2024-06-13 16:57:29 -0500

    feat(iam): added authentication interceptor to request pipeline.

--- src/shared/services/http-common.js
+++ src/shared/services/http-common.js
@@ -1,11 +1,15 @@
 import axios from "axios";
+import {authenticationInterceptor} from "../../iam/services/authentication.interceptor.js";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const http = axios.create({
     baseURL: API_BASE_URL,
-    headers: { 'Content-Type': 'application/json'}
+    headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'}
 });
+// Add authentication interceptor
+http.interceptors.request.use(authenticationInterceptor);
 
+// Export the http object
 export default http;
 
