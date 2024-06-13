0592b58a31afd7598d84524dd6a82e53b4045ef4
Angel Velasquez <pcisavel@upc.edu.pe>
2024-06-13 16:42:40 -0500

    feat(iam): added sign-in and sign-up navigation routes, and activated guard.

--- src/router/index.js
+++ src/router/index.js
@@ -7,15 +7,27 @@ import {createRouter, createWebHistory} from "vue-router";
 import HomeComponent from "../public/pages/home.component.vue";
 import AboutComponent from "../public/pages/about.component.vue";
 import TutorialManagementComponent from "../learning/pages/tutorial-management.component.vue";
+import SignInComponent from "../iam/pages/sign-in.component.vue";
+import SignUpComponent from "../iam/pages/sign-up.component.vue";
+import {authenticationGuard} from "../iam/services/authentication.guard.js";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: "/home",    component: HomeComponent, meta: { title: "Home"}},
-        { path: '/tutorials', component: TutorialManagementComponent, meta: { title: 'Tutorials' } },
-        { path: "/about",   component: AboutComponent, meta: { title: "About us"}},
+        { path: "/home",    name: 'home',       component: HomeComponent,               meta: { title: "Home"}},
+        { path: '/tutorials',                   component: TutorialManagementComponent, meta: { title: 'Tutorials' } },
+        { path: '/sign-in', name: 'sign-in',    component: SignInComponent,             meta: { title: 'Sign In' } },
+        { path: '/sign-up', name: 'sign-up',    component: SignUpComponent,             meta: { title: 'Sign Up' } },
+        { path: "/about",                       component: AboutComponent,              meta: { title: "About us"}},
         { path: "/",        redirect: "/home"},
     ]
+});
+
+router.beforeEach((to, from, next) => {
+    let baseTitle = 'ACME Learning Center';
+    document.title = `${baseTitle} | ${to.meta['title']}}`;
+    // Call the authentication guard
+    authenticationGuard(to, from, next);
 });
 
 export default router;
