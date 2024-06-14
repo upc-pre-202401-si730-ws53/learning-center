9af7c58d6d3af5151d292dfda8bb76cc92d56653
Angel Velasquez <pcisavel@upc.edu.pe>
2024-06-14 16:36:39 -0500

    fix: replaced function call by getter reference.

--- src/iam/services/authentication.interceptor.js
+++ src/iam/services/authentication.interceptor.js
@@ -2,7 +2,7 @@ import {useAuthenticationStore} from "./authentication.store.js";
 
 export const authenticationInterceptor = (config) => {
     const authenticationStore = useAuthenticationStore();
-    const isSignedIn = authenticationStore.isSignedIn();
+    const isSignedIn = authenticationStore.isSignedIn;
     if (isSignedIn) {
         config.headers.Authorization = `Bearer ${authenticationStore.currentToken}`;
         console.log(config);
